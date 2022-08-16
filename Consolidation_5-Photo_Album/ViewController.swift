4c89ba7dee8b9ab0a39a07268759421523d61851
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-08-16 15:08:08 +0200

    Initial Commit

---
+++ Consolidation_5-Photo_Album/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Consolidation_5-Photo_Album
+//
+//  Created by Edwin Przeźwiecki Jr. on 16/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
