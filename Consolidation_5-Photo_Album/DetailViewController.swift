aef8270f775c79e399bf2f55bd2ec2b5a5719bf2
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-08-16 16:59:16 +0200

    Start
    Initial storyboard settings and first lines of functions.

---
+++ Consolidation_5-Photo_Album/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  Consolidation_5-Photo_Album
+//
+//  Created by Edwin Przeźwiecki Jr. on 16/08/2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var photoView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
--- Consolidation_5-Photo_Album/ViewController.swift
+++ Consolidation_5-Photo_Album/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var photos = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Photo Album"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(takeAPhoto))
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Photo", for: indexPath)
+        //let photo = photos[indexPath.row]
+        cell.textLabel?.text = "Unknown"
+        cell.detailTextLabel?.text = "Unknown"
+        return cell
+    }
+    
+    @objc func takeAPhoto() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+    }
 }
 
